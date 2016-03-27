fba1ec0055ad809f7cdcf9b5d5b4d112fdddfeeb
didiercauvin <didier.cauvin@gmail.com>
2016-03-27 17:51:23 +0200

    add properties Cra

--- src/api/models/craModel.ts
+++ src/api/models/craModel.ts
@@ -7,10 +7,22 @@ const craSchema = new Schema({
     name: {type: String},
     month: {type: Number},
     year: {type: Number},
+    workingDays: {type: Number},
+    nonWorkingDays: {type: Number},
+    workingTimeReduction: {type: Number},
+    holidays: {type: Number},
+    unpaidLeave: {type: Number},
+    sickLeave: {type: Number},
+    exceptionalLeave: {type: Number},
+    intercontract: {type: Number},
+    training: {type: Number},
+    costs: {type: Number},
+    comments: {type: String},
     clientId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Client'
-    }
+    },
+    file: {data: Buffer, contentType: String}
 });
 
 export = craSchema;
